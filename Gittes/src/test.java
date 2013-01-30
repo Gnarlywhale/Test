15964807c0165c266009afd7c1146a7c4b4fddab
trd <trd@ug08.cs.ualberta.ca>
2013-01-29 17:41:32 -0700

    Try to grab this

---
+++ Gittes/src/test.java
@@ -0,0 +1,16 @@
+
+
+public class test
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("dolla dolla bill yall");
+		// TODO Auto-generated method stub
+
+	}
+
+}
